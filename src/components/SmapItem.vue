70c4c6053553af5df93201f8ff60cc6d6eeb0849
Moritz Hesse <moritz.hesse@smapone.com>
2023-06-22 14:40:17 +0200

    Implement set and remove webhook functionality

--- src/components/SmapItem.vue
+++ src/components/SmapItem.vue
@@ -1,42 +1,82 @@
 <template>
-    <div v-if="smap" class="smap" :class="{
-        webhook: smap.webhook,
-        fetching: smap.webhook === null,
-        'no-webhook': smap.webhook === false,
-    }">
+    <div
+        v-if="smap"
+        class="smap"
+        :class="{
+            webhook: smap.webhook,
+            fetching: smap.webhook === null,
+            'no-webhook': smap.webhook === false,
+        }"
+    >
         <div class="smap-name" @click="showMeta = !showMeta">
             {{ smap.name }}
             <span class="info-no-webhook" v-if="smap.webhook === false">no webhook</span>
         </div>
-        
+
         <div class="info-fetching" v-if="smap.webhook === null">fetching...</div>
 
         <div class="smap-meta" v-if="showMeta">
-            <div class="smap-meta-box">smapID <code>{{ smap.smapId }}</code></div>
-            
             <div class="smap-meta-box">
-                <div>Last changed <code>{{ getDate(smap.lastChanged) }}</code></div>
-                <div v-if="smap.lastPublishedVersion">Version <code>{{ smap.lastPublishedVersion.version }}</code></div>
+                smapID <code>{{ smap.smapId }}</code>
             </div>
-            
+
+            <div class="smap-meta-box">
+                <div>
+                    Last changed <code>{{ getDate(smap.lastChanged) }}</code>
+                </div>
+                <div v-if="smap.lastPublishedVersion">
+                    Version <code>{{ smap.lastPublishedVersion.version }}</code>
+                </div>
+            </div>
+
             <div class="smap-meta-box smap-meta-cols">
-                <div class="smap-meta-box">Licenses <code>{{ smap.userLicenseCount }}</code></div>
-                <div class="smap-meta-box">Installations <code>{{ smap.installationsCount }}</code></div>
-                <div class="smap-meta-box">Data entries <code>{{ smap.totalDataCount }}</code></div>
+                <div class="smap-meta-box">
+                    Licenses <code>{{ smap.userLicenseCount }}</code>
+                </div>
+                <div class="smap-meta-box">
+                    Installations <code>{{ smap.installationsCount }}</code>
+                </div>
+                <div class="smap-meta-box">
+                    Data entries <code>{{ smap.totalDataCount }}</code>
+                </div>
             </div>
 
             <div v-if="smap.webhook" class="smap-meta-box">
                 <div>Webhook URL</div>
-                <div><input type="text" :value="smap.webhook.serverUrl"></div>
+                <div><input type="text" :value="smap.webhook.serverUrl" /></div>
+            </div>
+
+            <div class="smap-meta-box smap-meta-cols">
+                <div class="set-webhook smap-meta-box" @click="setWebhook()">
+                    Set new webhook
+                </div>
+
+                <div
+                    v-if="smap.webhook"
+                    class="remove-webhook smap-meta-box"
+                    @click="removeWebhook()"
+                >
+                    Remove webhook
+                </div>
             </div>
         </div>
 
         <div v-if="smap.webhook">
             <div v-if="smap.webhook.options">
                 <div>
-                    <code v-if="smap.webhook.options === 'HttpGet'" class="method method-get">get</code>
-                    <code v-if="smap.webhook.options === 'HttpPost'" class="method method-post">post</code>
-                    <span class="webhook-url">{{ getReadableUrl(smap.webhook.serverUrl) }}</span>
+                    <code
+                        v-if="smap.webhook.options === 'HttpGet'"
+                        class="method method-get"
+                        >get</code
+                    >
+                    <code
+                        v-if="smap.webhook.options === 'HttpPost'"
+                        class="method method-post"
+                        >post</code
+                    >
+                    <span class="webhook-url">{{
+                        getReadableUrl(smap.webhook.serverUrl)
+                    }}</span>
                 </div>
             </div>
         </div>
@@ -46,37 +86,80 @@
 <script>
 export default {
     name: 'SmapItem',
-    emits: ["setWebhook"],
+    emits: ['displayWebhook'],
     // components: {},
     data() {
         return {
-            showMeta: false
-        }
+            showMeta: false,
+        };
     },
     props: {
         api: Object,
-        smap: Object
+        smap: Object,
     },
     async created() {
-        let data = await this.api.getWebhook(this.smap.smapId)
-        if(data === null) {
-            this.$emit('setWebhook', this.smap.smapId, false)
+        let data = await this.api.getWebhook(this.smap.smapId);
+        if (data === null) {
+            this.$emit('displayWebhook', this.smap.smapId, false);
         } else {
-            this.$emit('setWebhook', this.smap.smapId, data)
+            this.$emit('displayWebhook', this.smap.smapId, data);
         }
     },
     methods: {
         getReadableUrl(url) {
-            const lead = url.substr(0, 32)
-            const trail = url.substr(url.length-16, url.length)
-            return `${lead}...${trail}`
+            let readableUrl = url;
+            if (url.length > 32 + 16) {
+                const lead = url.substr(0, 32);
+                const trail = url.substr(url.length - 16, url.length);
+                readableUrl = `${lead}...${trail}`;
+            }
+            return readableUrl;
         },
         getDate(date) {
-            const options = { year: 'numeric', month: 'long', day: 'numeric' }
-            return new Date(date).toLocaleDateString("en-US", options)
-        }
-    }
-}
+            const options = { year: 'numeric', month: 'long', day: 'numeric' };
+            return new Date(date).toLocaleDateString('en-US', options);
+        },
+        async setWebhook() {
+            const method = 'HttpPost';
+            const target = prompt('Target URL (must be https)');
+
+            if (this.isValidHttpUrl(target) !== false && target != null) {
+                const result = await this.api.setWebhook(
+                    this.smap.smapId,
+                    target,
+                    method
+                );
+                if (result.status == 204) {
+                    this.$emit('displayWebhook', this.smap.smapId, {
+                        serverUrl: target,
+                        options: method,
+                    });
+                }
+            } else {
+                alert('Sorry, this is no valid URL. Please try again.');
+            }
+        },
+        async removeWebhook() {
+            if (confirm('Sure?') == true) {
+                const result = await this.api.removeWebhook(this.smap.smapId);
+                if (result.status == 204) {
+                    this.$emit('displayWebhook', this.smap.smapId, false);
+                }
+            }
+        },
+        isValidHttpUrl(string) {
+            let url;
+
+            try {
+                url = new URL(string);
+            } catch (_) {
+                return false;
+            }
+
+            return url.protocol === 'https:';
+        },
+    },
+};
 </script>
 
 <style scoped>
@@ -86,13 +169,13 @@ export default {
 }
 
 .smap:hover {
-    background-color: rgba(0, 0, 0, .5);
+    background-color: rgba(0, 0, 0, 0.5);
 }
 
 .smap-name {
     font-size: 1.5rem;
     cursor: pointer;
-    margin-bottom: .5rem;
+    margin-bottom: 0.5rem;
 }
 
 .smap-meta {
@@ -115,13 +198,13 @@ export default {
 }
 
 .smap-meta-cols code {
-    font-size: 2.3rem
+    font-size: 2.3rem;
 }
 
 .smap-meta-box {
     padding: 1rem;
     background: black;
-    margin: .5rem 0;
+    margin: 0.5rem 0;
 }
 
 .smap-meta-cols .smap-meta-box {
@@ -133,8 +216,22 @@ export default {
 }
 
 /* .webhook {} */
+.set-webhook,
+.remove-webhook {
+    padding: 1rem;
+    cursor: pointer;
+}
 
-.fetching, .no-webhook {
+.set-webhook {
+    background: rgba(255, 255, 255, 0.15);
+}
+
+.remove-webhook {
+    background: red;
+}
+
+.fetching,
+.no-webhook {
     color: #999;
 }
 
@@ -149,7 +246,7 @@ export default {
 }
 
 .webhook-url {
-    margin-left: .4rem;
+    margin-left: 0.4rem;
     font-size: 1.2rem;
 }
 </style>
--- src/smapjs/Api.js
+++ src/smapjs/Api.js
@@ -18,6 +18,38 @@ class Api {
         let response = await fetch(url)
         return await response.json()
     }
+
+    async setWebhook(smapId, target, method) {
+        let url = `https://platform.smapone.com/Backend/intern/Smaps/${smapId}/Notification/Data/Webhook?accessToken=${this.token}`
+        let options = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                ServerUrl: target,
+                Options: method
+            }),
+        }
+        try {
+            let response = await fetch(url, options)
+            return response
+        } catch (error) {
+            console.log({ error });
+        }
+    }
+
+    async removeWebhook(smapId) {
+        let url = `https://platform.smapone.com/Backend/intern/Smaps/${smapId}/Notification/Data/Webhook?accessToken=${this.token}`
+        let options = {
+            method: 'DELETE',
+            // headers: { 'Content-Type': 'application/json' }
+        }
+        try {
+            let response = await fetch(url, options)
+            return response
+        } catch (error) {
+            console.log({ error })
+        }
+    }
 }
 
-module.exports = Api;
+module.exports = Api
